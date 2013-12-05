1d041fe07b4b93488901e97bfa535f513e03e431
Fredrik Lind <efiiln-0@A3402.research.ltu.se>
2013-12-05 14:48:55 +0100

    Now linking pads

--- Lab2/mainwindow.cpp
+++ Lab2/mainwindow.cpp
@@ -92,16 +92,26 @@ void MainWindow::on_pushButton_clicked()
 
     // Set attributes
     g_object_set(G_OBJECT(overlay), "font-desc","Sans 24", "text","CAM1", "valign","top", "halign","left", "shaded-background", true, NULL);
-
-
     gst_bin_add_many (GST_BIN (pipeline), source, overlay, sink, NULL);
 
-    if (!gst_element_link_many (source, overlay, sink, NULL)) {
-        qDebug() << "Could not link elements";
-    }
+
+    // Get pads
+    //GstPad *autovideosrc_src, *textoverlay_video_sink, *textoverlay_src, *autovideosink_src;
+
+   /* autovideosrc_src = gst_element_get_static_pad(source, "src");
+    textoverlay_video_sink = gst_element_get_static_pad(overlay, "video-sink");
+    textoverlay_src = gst_element_get_static_pad(overlay, "src");
+    autovideosink_src = gst_element_get_static_pad(sink, "src");*/
+
+    gst_element_link_pads(source, "src", overlay, "video-sink");
+    gst_element_link_pads(overlay, "src", sink, "sink");
+
+    // Link pads
+    //gst_pad_link(autovideosrc_src, textoverlay_video_sink);
+
 
     gst_element_set_state (pipeline, GST_STATE_PLAYING);
-    GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(pipeline), GST_DEBUG_GRAPH_SHOW_ALL, "/afs/ltu.se/students/e/f/efiiln-0/M7017E-Lab2/dot-files/pipeline.dot");
+    GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(pipeline), GST_DEBUG_GRAPH_SHOW_, "pipeline");
     //g_object_set(G_OBJECT(), "port", "6000", NULL);
 }
 
